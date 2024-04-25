af7cba7616c84cca20a9edffa68affa70a8ca585
Magdalina1 <majdalol95@gmail.com>
2024-04-25 16:11:31 +0000

    add S to user

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
 BEGIN
 	DECLARE w_avg_score FLOAT;
 	SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight)
-				FROM user AS U
+				FROM users AS U
 				JOIN corrections as C ON U.id=C.user_id
 				JOIN projects AS P ON C.project_id=P.id
 				WHERE U.id=user_id);
