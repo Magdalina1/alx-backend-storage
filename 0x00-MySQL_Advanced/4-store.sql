655fc0c5c556440ee3e1e92907ca7204436fd9ff
Magdalina1 <majdalol95@gmail.com>
2024-04-25 13:12:03 +0000

    fix the code

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS orders;
 
 CREATE TABLE IF NOT EXISTS items (
-	name varchar(255) NOT NULL,
+	name VARCHAR(255) NOT NULL,
 	quantity int NOT NULL DEFAULT 10
 );
 
 CREATE TABLE IF NOT EXISTS orders (
-	item_name varchar(255) NOT NULL,
-	number int not null
+	item_name VARCHAR(255) NOT NULL,
+	number int NOT NULL
 );
 
 INSERT INTO items (name) VALUES ("appel"), ("pineapple"), ("pear");
