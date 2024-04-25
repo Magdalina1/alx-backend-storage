c08df69d425937f393dcf4df1b08d92aa57e2334
Magdalina1 <majdalol95@gmail.com>
2024-04-25 16:35:51 +0000

    fix the code

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -6,8 +6,8 @@ BEGIN
 	UPDATE users AS U,
 		(SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
 		FROM users AS U
-		JOIN correctins as C ON U.id=C.user_id
-		JOIN projects as P ON C.project_id=P.id
+		JOIN corrections as C ON U.id=C.user_id
+		JOIN projects AS P ON C.project_id=P.id
 		GROUP BY U.id)
 	AS WA
 	SET U.average_score = WA.w_avg
