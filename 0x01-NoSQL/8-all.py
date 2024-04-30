c2dc2a5143dff7265ebdcc8489bece577e9e1ec2
Magdalina1 <majdalol95@gmail.com>
2024-04-30 05:08:23 -0700

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Write a Python function that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """Return list of all docs in collection"""
+    if not mongo_collection:
+        return []
+    docs = mongo_collection.find()
+    return [doc for doc in docs]
